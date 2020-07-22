2c668e8c3418e2bcdedec936b7b7f3ccd609864c
Nikolay0107 <nimphonoe@gmail.com>
2020-07-22 14:42:30 +0300

    Added some files

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+
+Для создания нового файла в структуре проекта используется комбинация VS: Ctrl+Shift+A 
+
+*/
+
+
 int main()
 {
  
+
+	setlocale(LC_ALL, "ru");
+
+
+
+
 	return 0;
 
 }

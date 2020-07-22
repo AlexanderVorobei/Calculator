81dcf9e1172281cdd2b8112c552c798aab01d660
Nikolay0107 <nimphonoe@gmail.com>
2020-07-22 16:27:11 +0300

    Update v. 1.000
    Works Well

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -17,11 +17,27 @@ using namespace std;
 
 int main()
 {
- 
+ 	setlocale(LC_ALL, "ru");
 
-	setlocale(LC_ALL, "ru");
+	Circle c(13);
+	Rectangle r(3, 5);
+	Square s(7);
 
+	cout << "Circle: " << endl;
+	cout << "Area: "<< c.Area() << endl;
+	cout << "Perimeter: "<< c.Perimeter() << endl;
+	cout << endl;
 
+	cout << "Rectangle: " << endl;
+	cout << "Area: " << r.Area() << endl;
+	cout << "Perimeter: " << r.Perimeter() << endl;
+	cout << endl;
+
+	cout << "Square: " << endl;
+	cout << "Area: " << s.Area() << endl;
+	cout << "Perimeter: " << s.Perimeter() << endl;
+	cout << "Diagonal: " << s.Diagonal() << endl;
+	cout << endl;
 
 
 	return 0;

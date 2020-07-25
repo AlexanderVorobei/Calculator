bbd16915624092c7fb9638e780a63b3d7f29a56f
AlexanderVorobei <amvorobei@gmail.com>
2020-07-25 18:55:11 +0300

    Finish

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "Logic.h"
 using namespace std;
 
 int main()
 {
- 
-	return 0;
-
+	double a, b;
+	char oper;
+	cout << "Hello! Please insert math operation: (a+b) " << endl;
+	cin >> a >> oper >> b;
+	cout << a << oper << b << "=";
+	Logic calculator(a, b, oper);
+	calculator.calculator(a, b, oper);
 }
